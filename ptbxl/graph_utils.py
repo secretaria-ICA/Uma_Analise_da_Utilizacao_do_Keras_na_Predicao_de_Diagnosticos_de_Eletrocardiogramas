a48a829c40796d7f597ac574a9e28ef6445096fc
Alaor Barroso de Carvalho Neto <alaorneto@gmail.com>
2021-07-22 20:06:02 -0300

    Adição do gráfico de classes e hamming loss.

--- ptbxl/graph_utils.py
+++ ptbxl/graph_utils.py
@@ -62,3 +62,24 @@ def scp_graph(scp_statements):
             pos[k] = new_pos
 
     return {'G': G, 'pos': pos, 'color': color}
+
+def hamming_score(y_true, y_pred, normalize=True, sample_weight=None):
+    '''
+    Compute the Hamming score (a.k.a. label-based accuracy) for the multi-label case
+    https://stackoverflow.com/q/32239577/395857
+    '''
+    acc_list = []
+    for i in range(y_true.shape[0]):
+        set_true = set( np.where(y_true[i])[0] )
+        set_pred = set( np.where(y_pred[i])[0] )
+        #print('\nset_true: {0}'.format(set_true))
+        #print('set_pred: {0}'.format(set_pred))
+        tmp_a = None
+        if len(set_true) == 0 and len(set_pred) == 0:
+            tmp_a = 1
+        else:
+            tmp_a = len(set_true.intersection(set_pred))/\
+                    float( len(set_true.union(set_pred)) )
+        #print('tmp_a: {0}'.format(tmp_a))
+        acc_list.append(tmp_a)
+    return np.mean(acc_list)
